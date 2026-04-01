99bb907bcbccc8a4087325509775a7252fe8b14d
focoemcanto-pixel <focoemcanto@gmail.com>
2026-03-31 21:23:16 -0300

    Create SuggestoesPage component with AdminShell

---
+++ app/sugestoes/page.js
@@ -0,0 +1,10 @@
+import AdminShell from '../../components/admin/AdminShell';
+import SuggestoesPageClient from '../../components/sugestoes/SuggestoesPageClient';
+
+export default function SuggestoesPage() {
+  return (
+    <AdminShell pageTitle="Sugestões" activeItem="sugestoes">
+      <SuggestoesPageClient />
+    </AdminShell>
+  );
+}

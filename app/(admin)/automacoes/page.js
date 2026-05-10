dadc0518bb20a7d339d783069f03c58939b914db
focoemcanto-pixel <focoemcanto@gmail.com>
2026-05-09 22:02:34 -0300

    feat: protect automacoes page by workspace module

--- app/(admin)/automacoes/page.js
+++ app/(admin)/automacoes/page.js
@@ -1,10 +1,13 @@
 import AdminShell from '@/components/layout/AdminShell';
 import AutomacoesPageClient from '@/components/automacoes/AutomacoesPageClient';
+import WorkspaceModuleGuard from '@/components/workspace/WorkspaceModuleGuard';
 
 export default function AutomacoesPage() {
   return (
-    <AdminShell pageTitle="Central de Automação" activeItem="automacoes">
-      <AutomacoesPageClient />
-    </AdminShell>
+    <WorkspaceModuleGuard moduleKey="automacoes" requireAdmin>
+      <AdminShell pageTitle="Central de Automação" activeItem="automacoes">
+        <AutomacoesPageClient />
+      </AdminShell>
+    </WorkspaceModuleGuard>
   );
 }

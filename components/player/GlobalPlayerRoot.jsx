5a1e0049d692b06839685b2ccccd12001990038a
focoemcanto-pixel <focoemcanto@gmail.com>
2026-08-27 08:39:39 -0300

    feat: mount Media Session bridge without touching stable player

--- components/player/GlobalPlayerRoot.jsx
+++ components/player/GlobalPlayerRoot.jsx
@@ -2,11 +2,13 @@
 
 import { GlobalPlayerProvider } from '@/components/player/GlobalPlayerProvider';
 import GlobalPlayerHostFixed from '@/components/player/GlobalPlayerHostFixed';
+import MediaSessionBridge from '@/components/player/MediaSessionBridge';
 
 export default function GlobalPlayerRoot({ children }) {
   return (
     <GlobalPlayerProvider>
       <GlobalPlayerHostFixed />
+      <MediaSessionBridge />
       {children}
     </GlobalPlayerProvider>
   );
